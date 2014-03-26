46677badd1b3c12675bf99d43a6218e8118598d0
victor oliveira <voliveira@tweasy.com>
2014-03-26 00:41:56 +0000

    Remove old

--- application/logs/2014/02/27.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2014-02-27 17:01:09 --- ERROR: ErrorException [ 4 ]: syntax error, unexpected end of file ~ APPPATH/views/home/home.php [ 607 ]
-2014-02-27 17:01:09 --- STRACE: ErrorException [ 4 ]: syntax error, unexpected end of file ~ APPPATH/views/home/home.php [ 607 ]
---
-#0 [internal function]: Kohana_Core::shutdown_handler()
-#1 {main}
--- application/logs/2014/03/09.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2014-03-09 18:04:07 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
-2014-03-09 18:04:07 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
---
-#0 /var/www/realstate/index.php(108): Kohana_Request->execute()
-#1 {main}
