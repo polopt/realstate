7d9310f981970a55a1acc413ea82ae5a7668a373
project-andyou <muffinbook@muffinbook.local>
2014-04-18 11:07:42 +0100

    meeting 140418

---
+++ application/logs/2014/02/10.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2014-02-10 19:31:17 --- ERROR: ReflectionException [ -1 ]: Class controller_shindig does not exist ~ SYSPATH/classes/kohana/request.php [ 1178 ]
+2014-02-10 19:31:17 --- ERROR: ReflectionException [ -1 ]: Class controller_shindig does not exist ~ SYSPATH/classes/kohana/request.php [ 1178 ]
+2014-02-10 19:31:17 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: media/js/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2014-02-10 19:31:47 --- ERROR: Database_Exception [ 8192 ]: mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead ~ MODPATH/database/classes/kohana/database/mysql.php [ 67 ]
+2014-02-10 19:31:47 --- ERROR: Database_Exception [ 8192 ]: mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead ~ MODPATH/database/classes/kohana/database/mysql.php [ 67 ]
+2014-02-10 19:31:47 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: media/js/jquery.min.js ~ SYSPATH/classes/kohana/request.php [ 676 ]
